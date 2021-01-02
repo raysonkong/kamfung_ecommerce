89f98df74f60b5e02891329cfe49e41692e3722b
Rayson Kong <kong.rayson@gmail.com>
2021-01-02 16:18:22 +0800

    install react-bootstrap and import bootswatch theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 const App = () => {
   return (
     <div>
-      <h1>Hello! This is KamFung</h1>
+      <Header />
+      <main>
+        <h1>Welcome to KamFung</h1>
+      </main>
+      <Footer />
     </div>
   );
 }
